93eaae238fd247f9708f8cedcaf2fb5114593b97
AndresBlanco57 <andres.blanco@jpschools.org>
2025-02-13 19:39:43 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,34 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+    createPlatform(190, 620, 310, 120);
+    createPlatform(1000, 620, 500, 120);
+    createPlatform(700, 520, 100, 220);
+    createPlatform(1040, 500, 180, 20);
+    createPlatform(1040, 300, 20, 200);
+    createPlatform(1250, 370, 290, 20);
+    createPlatform(1390, 0, 20, 700);
+    createPlatform(690, 300, 120, 20);
+    createPlatform(150, 260, 350, 20);
+    createPlatform(150, 0, 20, 280);
+    createPlatform(150, 170, 150, 90);
     // TODO 3 - Create Collectables
+    createCollectable("diamond", 200, 110, 0, 0.7);
+    createCollectable("diamond", 730, 430, 0, 0.7);
+    createCollectable("diamond", 1200, 410, 0, 0.7);
 
 
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("bottom", 530, 800);
+    createCannon("bottom", 550, 800);
+    createCannon("bottom", 850, 750);
+    createCannon("bottom", 830, 750);
     
     
     //////////////////////////////////
